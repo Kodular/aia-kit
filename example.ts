0f4c313d326da44e9972d8a51fd60bcd6488cc26
Pavitra Golchha <hello@pavi2410.me>
2024-11-08 21:12:10 +0530

    Add example

---
+++ example.ts
@@ -0,0 +1,37 @@
+import { AIAReader } from './src/aia_reader';
+import fs from "node:fs/promises";
+
+async function main() {
+    try {
+        const aiaFile = await fs.readFile('test/fixtures/Test.aia')
+        const aiaFileBlob = new Blob([aiaFile])
+        
+        // Create an instance of AIAReader and read the AIA file
+        const project = await AIAReader.parse(aiaFileBlob);
+
+        // Pretty print the parsed information
+        console.log('Project Information:');
+        console.log('===================');
+        console.log(`Name: ${project.name}`);
+
+        
+        // Print screens information
+        console.log('\nScreens:');
+        console.log('========');
+        project.screens.forEach(screen => {
+            console.log(`\nScreen: ${screen.name}`);
+        });
+
+        // Print assets information
+        console.log('\nAssets:');
+        console.log('=======');
+        project.assets.forEach(asset => {
+            console.log(`- ${asset.name}`);
+        });
+
+    } catch (error) {
+        console.error('Error parsing AIA file:', error);
+    }
+}
+
+main();
