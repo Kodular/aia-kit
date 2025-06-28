ce374130094dfe36f264ed71b218294f7a7005b6
Pavitra Golchha <hello@pavi2410.me>
2025-06-28 17:08:03 +0530

    test: Enhance AIAReader tests with additional property and screen validations

--- test/aia.test.ts
+++ test/aia.test.ts
@@ -15,8 +15,24 @@ describe("AIAReader", () => {
     expect(project).toBeDefined();
 
     expect(project.name).toBe("Test");
+    expect(project.properties).toBeDefined();
+    expect(project.properties).toMatchObject({
+      aname: "Test",
+      main: "io.kodular.diego_barreiro_perez.Test.Screen1",
+      minSdk: "21",
+      name: "Test",
+      versioncode: "1",
+      versionname: "1.0",
+    });
 
     expect(project.screens.length).toBe(1);
     expect(project.screens[0].name).toBe("Screen1");
+
+    expect(project.screens[0].form.name).toBe("Screen1");
+    expect(project.screens[0].form.type).toBe("Form");
+
+    expect(project.assets.length).toBe(0);
+
+    expect(project.extensions.length).toBe(0);
   });
 });
