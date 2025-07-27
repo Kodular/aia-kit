4cbae2e91d6cd5a79f4e5eb22ce9c09db17edc61
Pavitra Golchha <hello@pavi2410.me>
2025-07-27 16:42:45 +0530

    export Environment API

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { Asset } from "./asset.js";
 export { Component } from "./component.js";
+export { Environment } from "./Environment.js";
 export { Extension } from "./extension.js";
 export { Project } from "./project.js";
 export { parseAia } from "./reader.js";
@@ -7,8 +8,8 @@ export { Screen } from "./screen.js";
 export { writeAia } from "./writer.js";
 
 // Parsers
-export { default as ScmParser } from "./parsers/ScmParser.js";
 export { BkyParser } from "./parsers/BkyParser.js";
+export { default as ScmParser } from "./parsers/ScmParser.js";
 
 // YAIL Generator
 export { default as YailGenerator } from "./generators/yail/YailGenerator.js";
