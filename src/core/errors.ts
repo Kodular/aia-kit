a5aa86656d54ef859bc98451e1e43118788a9fe3
Pavitra Golchha <hello@pavi2410.com>
2026-05-02 01:37:48 +0530

    feat(v2): add error hierarchy

---
+++ src/core/errors.ts
@@ -0,0 +1,36 @@
+export class AiaKitError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'AiaKitError'
+  }
+}
+
+export class AiaParseError extends AiaKitError {
+  cause: unknown
+  constructor(message: string, cause: unknown) {
+    super(message)
+    this.name = 'AiaParseError'
+    this.cause = cause
+  }
+}
+
+export class AiaZipError extends AiaParseError {
+  constructor(message: string, cause: unknown) {
+    super(message, cause)
+    this.name = 'AiaZipError'
+  }
+}
+
+export class AiaStructureError extends AiaParseError {
+  constructor(message: string, cause: unknown) {
+    super(message, cause)
+    this.name = 'AiaStructureError'
+  }
+}
+
+export class AiaWriteError extends AiaKitError {
+  constructor(message: string) {
+    super(message)
+    this.name = 'AiaWriteError'
+  }
+}
